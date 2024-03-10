45acb2de82e62cf4c1f1449db5f9e7b1605eae76
Mariia Gaidomak <mariia.gaidomak@rokolabs.com>
2024-03-10 23:23:55 +0100

    Add base classes

---
+++ RecipeManager.API/RecipeManager.Data/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeManager.Data
+{
+	public class Comment
+	{
+        public Guid Id { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid CreatedFor { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public required string Text { get; set; }
+    }
+}
